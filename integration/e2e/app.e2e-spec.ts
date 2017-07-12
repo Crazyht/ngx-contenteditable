3a5f3efcc07134847c55a7756c03e33aae8504d9
Hervé TINANT <herve.tinant@finquest.com>
2017-07-12 03:49:09 +0200

    fix integration test

--- integration/e2e/app.e2e-spec.ts
+++ integration/e2e/app.e2e-spec.ts
@@ -1,11 +1,8 @@
 import { browser, element, by } from 'protractor';
-import { AppComponent } from '../src/app/app.component';
 
 describe('Ngx-ContentEditable E2E tests', function () {
 
-  beforeEach(function() {
-   this.app = new AppComponent();
- });
+  beforeEach(() => browser.get(''));
 
   afterEach(() => {
     browser.manage().logs().get('browser').then((browserLog: any[]) => {
